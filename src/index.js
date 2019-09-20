9cd580724b15423291ecdffa0b2959b4e5a6a955
eaTong <eatongchou@gmail.com>
2019-09-20 22:04:13 +0800

    initial project

---
+++ src/index.js
@@ -0,0 +1,50 @@
+'use strict';
+const {extractVariableSelection, getScriptTemplate} = require('./utils');
+const defaultOption = {
+  fileName: 'webpack-variable-replacer.js',
+  matchVariables: {},
+  publicPath: '',
+  htmlFileName: 'index.html',
+  buildPath: ''
+};
+
+class VariableReplacer {
+  constructor(options) {
+    this.options = {...defaultOption, ...options};
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tapAsync('VariableReplacer', (compilation, callback) => {
+      const assets = compilation.assets;
+      const {matchVariables} = this.options;
+      let templateString = '';
+      Object.keys(assets).forEach((key) => {
+        if (/\.css$/.test(key)) {
+          const styleAsset = assets[key];
+          templateString += extractVariableSelection(styleAsset.children ? styleAsset.children[0]._value : assets[key]._value, matchVariables);
+        }
+      });
+      const {fileName, buildPath} = this.options;
+      const output = `${buildPath}${fileName}`;
+      templateString = getScriptTemplate(matchVariables, templateString);
+      compilation.assets[output] = {
+        source: () => templateString,
+        size: () => templateString.length
+      };
+      this.injectToHTML(compilation);
+      callback();
+    });
+  }
+
+  injectToHTML(compilation) {
+    const {publicPath, fileName, htmlFileName} = this.options;
+    const htmlAsset = compilation.getAsset(htmlFileName);
+    const htmlTemp = htmlAsset.source.source().replace(`</body>`, `<script type="text/javascript" src="${publicPath + fileName}"></script></body>`);
+    compilation.assets[htmlFileName] = {
+      source: () => htmlTemp,
+      size: () => htmlTemp.length
+    }
+  }
+}
+
+module.exports = VariableReplacer;
---
+++ src/utils.js
@@ -0,0 +1,77 @@
+const CSS_REG = /[^}]*\{[^{]*\}/g;
+const RULE_REG = /(\w+-)*\w+:/;
+const CSS_VALUE_REG = /(\s?[a-z0-9]+\s)*/;
+const SAFE_EMPTY_REG = /\s?/;
+
+function extractVariableSelection(styles, matchVariables) {
+  styles = styles.replace(/\r|\t|\n/g, '');
+  const matchVariableValues = Object.keys(matchVariables).map(item => matchVariables[item]);
+  const valueKey = reverseKeyValue(matchVariables);
+  const cssExpressions = styles.match(CSS_REG);
+  let allExtractedVariable = '';
+  const variableReg = getVariablesReg(matchVariableValues);
+  if (!cssExpressions || cssExpressions.length === 0) {
+    return '';
+  }
+  for (const expression of cssExpressions) {
+    if (variableReg.test(expression)) {
+      const selector = expression.match(/[^{]*/)[0];
+      if (/^@.*keyframes/.test(selector)) {
+        allExtractedVariable += `${selector}{${ extractVariableSelection(expression.replace(/[^{]*\{/, '').replace(/}$/, ''), matchVariables)}}`;
+      } else {
+        const colorRules = expression.match(combineRegs('g', RULE_REG, SAFE_EMPTY_REG, CSS_VALUE_REG, variableReg));
+        if (colorRules) {
+          const colorReplaceTemplates = colorRules.map(item => item.replace(variableReg, str => `VARIABLE_REPLACE_${valueKey[str]}`));
+          allExtractedVariable += `${selector}{${colorReplaceTemplates.join(';')}}`;
+        }
+      }
+    }
+  }
+  return allExtractedVariable;
+}
+
+function reverseKeyValue(obj) {
+  const temp = {};
+  Object.keys(obj).forEach(key => {
+    temp[obj[key].replace(/\s/g, '').replace(/0?\./, '.')] = key;
+  });
+  return temp;
+}
+
+function getVariablesReg(colors) {
+  return new RegExp(colors.map(i => `(${i.replace(/\s/g, ' ?').replace(/\(/g, `\\(`).replace(/\)/g, `\\)`).replace(/0?\./g, `\\.`)})`).join('|'));
+}
+
+function combineRegs(decorator = '', ...args) {
+  const regString = args.map(item => {
+    const str = item.toString();
+    return `(${str.slice(1, str.length - 1)})`
+  }).join('');
+  return new RegExp(regString, decorator);
+}
+
+function getScriptTemplate(matchVariables, styleStr) {
+  return `
+window.replaceStyleVariable = function (replaceVariables) {
+  var option = JSON.parse('${JSON.stringify(matchVariables)}');
+  for(var key in replaceVariables){
+    option[key] = replaceVariables[key];
+  }
+  var str = '${styleStr}';
+  var unionId = '${Math.random() + new Date().getTime()}';
+  var style = document.getElementById(unionId);
+  if (!style) {
+    style = document.createElement('style');
+    style.id = unionId;
+    document.head.appendChild(style);
+  }
+  for (var key in replaceVariables) {
+    var reg = new RegExp('VARIABLE_REPLACE_' + key+'\\\\b', 'g');
+    str = str.replace(reg, option[key]);
+  }
+  style.innerText = str;
+}
+`
+}
+
+module.exports = {extractVariableSelection, getScriptTemplate};
